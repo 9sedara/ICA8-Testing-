97b2942b9ecf811a72fe7de64ffe1cf97b28611b
9sedara <sedara9@asu.edu>
2022-10-27 15:24:14 -0700

    ICA-8

---
+++ ICA8_Testing/src/main/java/urinals.java
@@ -0,0 +1,15 @@
+
+
+/*Author
+    Sai Susmita Edara
+ */
+
+
+public class urinals {
+
+    public static void main(String[] args) {
+
+        System.out.println("Author:Sai Susmita Edara");
+    }
+
+}
---
+++ ICA8_Testing/src/test/java/urinals_test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+public class urinals_test {
+    @Test
+    public void Test(){
+            fail("not implemented");
+    }
+
+
+}
