a6e1c563ce50363db72ba4ea215cbd117231ce79
9sedara <sedara9@asu.edu>
2022-10-27 19:37:51 -0700

    updated files

--- ICA8_Testing/src/main/java/urinals.java
+++ ICA8_Testing/src/main/java/urinals.java
@@ -1,113 +1,15 @@
-import java.util.Scanner;
+
 
 //Author:Sai Susmita Edara
 
 public class urinals {
+
     public static void main(String[] args) {
-        String str= getString();
-        boolean a=goodString(str);
-        if(a==false)
-        {
-            System.out.println("invalid string, print -1");
-        }
-        else {
-            int b=urinalscount(str);
-            System.out.println(b);
-        }
-
     }
-    public static boolean goodString(String str){
-        for(int i=0;i<str.length();i++)
+        public static boolean goodString(String str)
         {
-            if(str.charAt(i)=='0' || str.charAt(i)=='1')
-            {
-                if(i>0 && str.charAt(i)=='1' && str.charAt(i-1)=='1')
-                {
-                    return false;
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static int urinalscount(String str)
-    {
-        int[]p=new int[str.length()];
-        for(int i=0;i<p.length;i++)
-        {
-            p[i]=0;
-        }
-        for(int i=0;i<str.length();i++)
-        {
-            if(i==0)
-            {
-                if(str.charAt(i)=='0')
-                {
-                    if(i+1<str.length()) {
-                        if (str.charAt(i + 1) == '1') {
-                            p[i] = 0;
-                        } else {
-                            p[i] = 1;
-                        }
-                    }
-                }
-                else {
-                    p[i]=0;
-                }
-            }
-            else if(i==1)
-            {
-                if(str.charAt(i)=='1') {
-                    p[i] = p[i - 1];
-                }
-                else
-                {
-                    if(str.charAt(i-1)=='1')
-                    {
-                        p[i]=p[i-1];
-                    }
-                    else if(i+1<str.length() && str.charAt(i+1)=='1')
-                    {
-                        p[i]=p[i-1];
-                    }
-                    else
-                    {
-                        p[i]=1;
-                    }
-                }
-            }
-            else {
-                p[i]=p[i-1];
-                if(str.charAt(i)=='1') {
-                    p[i] = p[i - 1];
-                }
-                else
-                {
-                    if(str.charAt(i-1)=='1')
-                    {
-                        p[i]=p[i-1];
-                    }
-                    else if(i+1<str.length() && str.charAt(i+1)=='1')
-                    {
-                        p[i]=p[i-1];
-                    }
-                    else
-                    {
-                        p[i]=Math.max(p[i],1+p[i-2]);
-                    }
-                }
-            }
-        }
-        return p[str.length()-1];
-    }
-    public static String getString(){
-        Scanner scr=new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String str=scr.next();
-        return str;
+            System.out.println("Not Yet Implemented");
+            return true;
     }
 
 }
--- ICA8_Testing/src/test/java/urinalsTest.java
+++ ICA8_Testing/src/test/java/urinalsTest.java
@@ -10,16 +10,9 @@ class urinalsTest {
 
     @Test
     void goodString() {
-        assertEquals(false,urinals.goodString("1000001111"));
+        assertEquals(false, urinals.goodString("100001"));
         System.out.println("======Sai Susmita Edara==Test Two Executed======");
 
     }
 
-    @Test
-    void urinalscount() {
-    }
-
-    @Test
-    void getString() {
-    }
 }
